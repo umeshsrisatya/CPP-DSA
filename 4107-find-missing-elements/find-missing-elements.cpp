505e40b0714dfcfd22347502e9ae4dcaa12ab7dc
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2025-11-02 17:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (44.44%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int> output;
-        for(int i = 1; i < nums.size(); i++){
-            if((nums[i-1] + 1) == nums[i]){
-                continue;
-            }
-            for(int j = nums[i-1] +1 ; j < nums[i]; j++){
-                output.push_back(j);
+        int n = nums.size();
+        
+        vector<int> Bit(101,0);
+        int s = INT_MAX;
+        int h = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            s = (s > nums[i]) ? nums[i] : s;
+            h = (h < nums[i]) ? nums[i] : h;
+            Bit[nums[i]] = 1;
+        }
+
+        vector<int> output; 
+        for(int i = s; i <= h ; i++){
+            if(Bit[i]==0){
+                output.push_back(i);
             }
         }
         return output;
