ef81f21604d8f7f1b842ab0dcf9e808c1d88ac8b
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-09-08 11:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string s = to_string(n);
-        int digits = 0;
-        for(char ch : s){
-            digits++;
-        }
-        if(digits<4){
-            return 0;
-        }
-        return n-999;
+        return n-999>0?n-999:0;
     }
 };
